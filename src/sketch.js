1b625a2c2381a65b05de49a0c7f8f2e4f4e66409
Alejandro bustos <alejandrob@live.cl>
2020-05-29 01:15:37 -0400

    Added rollup Added debugging Added building Added first example

---
+++ src/sketch.js
@@ -0,0 +1,43 @@
+import tokens from './tokens'
+
+const sketch = (p) => {
+  let xoff = 0
+  let xincrement = 0.001
+
+  let height
+  let width
+
+  p.setup = () => {
+    height = p.windowHeight
+    width = p.windowWidth
+    p.createCanvas(width, height)
+    p.pixelDensity(1)
+    p.background(5)
+    p.noStroke()
+  }
+
+  p.draw = () => {
+    p.loadPixels()
+    // p.background(tokens.color1)
+    // p.stroke(50)
+    // p.noFill()
+    // p.beginShape()
+    for(var x=0; x < width; x++) {
+      for(var y=0; y < height; y++) {
+        const index = (x+y*width) * 4
+        const r = p.random(255)
+        p.pixels[index+0] = r
+      }
+    }
+    // p.endShape();
+    p.updatePixels();
+  }
+
+  //p.windowResized = () => {
+  //  p.clear()
+  //  p.resizeCanvas(p.windowwidth, p.windowheight)
+  //  p.draw()
+  //}
+}
+
+export default sketch
