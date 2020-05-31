09a26aa0fd8dd8694c1988dab98246e5b72f9028
Alejandro bustos <alejandrob@live.cl>
2020-05-30 23:49:11 -0400

    Updating style import Fixing some styles Removing reset

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import P5 from 'p5/lib/p5.min'
 import sketch from './sketch'
 import './index.css'
-import './reset.css'
+// import './reset.css'
 
 new P5(sketch, document.getElementById('sketch'))
