99300cd58ad230a65db24aed067d76e2fbcad151
Alejandro Bustos Nuñez <alejandro@Alejandros-MacBook-Pro.local>
2020-06-17 23:56:41 -0400

    Migration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-// import Animation from './example1'
-import Animation from './example2'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-Animation.init();
-Animation.render();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
