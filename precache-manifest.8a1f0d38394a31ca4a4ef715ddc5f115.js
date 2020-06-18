baa3dd1bd494f85483c344ed1cd7c30b510b5465
Alejandro Bustos Nuñez <alejandro@Alejandros-MacBook-Pro.local>
2020-06-18 00:02:06 -0400

    Updates

---
+++ precache-manifest.8a1f0d38394a31ca4a4ef715ddc5f115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f0e7beb0a54e5209bfdd8e75e0c685",
+    "url": "/animation/index.html"
+  },
+  {
+    "revision": "3521bff85038568ce39d",
+    "url": "/animation/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bf3605eb14b42ffffa26",
+    "url": "/animation/static/js/2.9120ebb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animation/static/js/2.9120ebb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3521bff85038568ce39d",
+    "url": "/animation/static/js/main.394c8da8.chunk.js"
+  },
+  {
+    "revision": "80a32e83b2bac39d3439",
+    "url": "/animation/static/js/runtime-main.5c9fc954.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animation/static/media/logo.5d5d9eef.svg"
+  }
+]);
